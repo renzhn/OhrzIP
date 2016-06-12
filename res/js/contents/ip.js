432045d81eb0581c932f44441e5c433d56123e87
任震 <renzhen1@staff.sina.com.cn>
2016-06-12 16:18:24 +0800

    no local check

--- res/js/contents/ip.js
+++ res/js/contents/ip.js
@@ -6,27 +6,7 @@ function tag(type, id, innerHTML) {
     return el;
 }
 
-function isLocal (ip) {
-    if (!ip) return false;
-    if (ip === 'localhost') return true;
-
-    ip = ip.split('.').map(function(oct) { return parseInt(oct, 10); });
-    // 127.0.0.1 - 127.255.255.255
-    if (ip[0] === 127) return true;
-    // 10.0.0.0 - 10.255.255.255
-    if (ip[0] === 10) return true;
-    // 172.16.0.0 - 172.31.255.255
-    if (ip[0] === 172 && ip[1] >= 16 && ip[1] <= 31) return true;
-    // 192.168.0.0 - 192.168.255.255
-    if (ip[0] === 192 && ip[1] === 168) return true;
-
-    return false;
-}
-
 function requestIPLocation(ip) {
-    if (isLocal(ip)) {
-        return;
-    }
     chrome.extension.sendMessage({'get': 'location', 'data': ip}, function (response) {
         if (response && response.location && ipDom) {
             ipDom.innerHTML = ipDom.innerHTML + response.location;
