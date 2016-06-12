44864be886a7bbe06cdcd488be0d3fb92b133154
任震 <renzhen1@staff.sina.com.cn>
2016-06-12 15:35:48 +0800

    Ohrz IP

--- res/js/background.js
+++ res/js/background.js
@@ -1,100 +1,81 @@
 var setting = {'status': 'on'};
 
-
 function saveSetting() {
-	chrome.storage.local.set({'status': setting['status']}, function() {}); // 保存到本地
+    chrome.storage.local.set({'status': setting['status']}, function () {});
 }
 
-
 function setStatus(status) {
-	if (status !== 'off') {
-		chrome.browserAction.setBadgeText({'text': 'on'});
-		chrome.browserAction.setBadgeBackgroundColor({'color': '#14892c'});
-
-		// 开始监听
-		currentIPList = {};
-		chrome.webRequest.onCompleted.addListener(onCompletedFunc , { urls: [], types: ['main_frame'] }, []);
-	}
-	else {
-		chrome.browserAction.setBadgeText({'text': 'off'});
-		chrome.browserAction.setBadgeBackgroundColor({'color': '#d04437'});
-
-		// 清楚监听
-		currentIPList = {};
-		chrome.webRequest.onCompleted.removeListener(onCompletedFunc);
-	}
-	saveSetting();
+    if (status !== 'off') {
+        chrome.browserAction.setBadgeText({'text': 'on'});
+        chrome.browserAction.setBadgeBackgroundColor({'color': '#14892c'});
+    } else {
+        chrome.browserAction.setBadgeText({'text': 'off'});
+        chrome.browserAction.setBadgeBackgroundColor({'color': '#d04437'});
+    }
+    saveSetting();
 }
 
 function toggleOnOff(tab) {
-	if (setting['status'] === 'on') {
-		setting['status'] = 'off';
-		
-	}
-	else {
-		setting['status'] = 'on';
-	}
-	setStatus(setting['status']);
+    if (setting['status'] === 'on') {
+        setting['status'] = 'off';
+    } else {
+        setting['status'] = 'on';
+    }
+    setStatus(setting['status']);
 }
 
-// 监听点击事件
+// 监听事件
+chrome.webRequest.onCompleted.addListener(onCompletedFunc, {urls: [], types: ['main_frame']}, []);
 chrome.browserAction.onClicked.addListener(toggleOnOff);
 
 // 回复状态
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	// 请求设置状态
-	if ('status' === request.get) {
-		sendResponse(setting);
-	}
-	else if ('ip' === request.get) {
-		var ip = 'null';
-		if (currentIPList[sender.tab.url]) {
-			ip = currentIPList[sender.tab.url];
-			delete currentIPList[sender.tab.url];
-		}
-		sendResponse({'ip': ip});
-	}
-	else if ('location' === request.get) {
-		var location = '';
-		http_ajax('http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=' + request.data, 'GET', false, function(data) {
-			if (true === data.success) {
-				try {
-					var loc = JSON.parse(data.content);
-					if (loc.ret !== undefined) {
-						if (loc.ret < 0) {
-							location = '内网或未分配IP';
-						}
-						else if(loc.ret == 1) {
-							location = loc.country + ' ' + loc.province + ' ' + loc.city;
-						}
-
-					}
-				} catch(exception) {
-
-				}
-				
-			}
-			else {
-				location = '';
-			}
-		});
-		sendResponse({'location': location});
-	}
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    // 请求设置状态
+    if ('ip' === request.get) {
+        var ip = 'null';
+        if (currentIPList[sender.tab.url]) {
+            ip = currentIPList[sender.tab.url];
+            delete currentIPList[sender.tab.url];
+        }
+        sendResponse({'ip': ip});
+    } else if ('location' === request.get) {
+        var location = '';
+        http_ajax('http://www.cz88.net/ip/index.aspx?ip=' + request.data, 'GET', false, function (data) {
+            if (data.success) {
+                try {
+                    var addr = data.content.match(/id="InputIPAddrMessage">(.*?)</);
+                    if (addr) {
+                        location += '<br />[' + addr[1] + ']';
+                    }
+                    if (setting['status'] === 'on') {
+                        var myIp = data.content.match(/id="cz_ip">(.*?)</);
+                        if (myIp) {
+                            location += '<br />我的IP ' + myIp[1];
+                        }
+                        var myAddr = data.content.match(/id="cz_addr">(.*?)</);
+                        if (myAddr) {
+                            location += '<br />[' + myAddr[1] + ']';
+                        }
+                    }
+                } catch (exception) {
+                    console.log(exception);
+                }
+            }
+        });
+        sendResponse({'location': location});
+    }
 });
 
-// 缓存IP，每次用完就清空
-var currentIPList	= {};
+var currentIPList = {};
 
 function onCompletedFunc(info) {
-	currentIPList[ info.url ] = info.ip;
-	return;
+    currentIPList[info.url] = info.ip;
+    return;
 }
 
-// 加载配置
-chrome.storage.local.get(['status'], function(data) {
-	if ('off' === data['status']) {
-		setting['status'] = 'off';
-	}
-	// 读取配置成功之后，显示图标
-	setStatus(setting['status']);
+chrome.storage.local.get(['status'], function (data) {
+    if ('off' === data['status']) {
+        setting['status'] = 'off';
+    }
+    setStatus(setting['status']);
 });
--- res/js/contents/ip.js
+++ res/js/contents/ip.js
@@ -1,41 +1,47 @@
 var ipDom = null;
-function tag(type, id, className, innerHTML) {
+function tag(type, id, innerHTML) {
     var el = document.createElement(type);
     el.id = id;
-    el.className = className;
     el.innerHTML = innerHTML;
     return el;
 }
 
+function isLocal (ip) {
+    if (!ip) return false;
+    if (ip === 'localhost') return true;
+
+    ip = ip.split('.').map(function(oct) { return parseInt(oct, 10); });
+    // 127.0.0.1 - 127.255.255.255
+    if (ip[0] === 127) return true;
+    // 10.0.0.0 - 10.255.255.255
+    if (ip[0] === 10) return true;
+    // 172.16.0.0 - 172.31.255.255
+    if (ip[0] === 172 && ip[1] >= 16 && ip[1] <= 31) return true;
+    // 192.168.0.0 - 192.168.255.255
+    if (ip[0] === 192 && ip[1] === 168) return true;
+
+    return false;
+}
+
 function requestIPLocation(ip) {
-    chrome.extension.sendMessage({'get': 'location', 'data': ip}, function(response) {
+    if (isLocal(ip)) {
+        return;
+    }
+    chrome.extension.sendMessage({'get': 'location', 'data': ip}, function (response) {
         if (response && response.location && ipDom) {
-                ipDom.innerHTML = ipDom.innerHTML + '<br />' + response.location;
-            }
+            ipDom.innerHTML = ipDom.innerHTML + response.location;
+        }
     });
 }
 
-chrome.extension.sendMessage({'get': 'status'}, function(response) {
-    if ('off' !== response.status) {
-        // 开启状态
-        chrome.extension.sendMessage({'get': 'ip'}, function(response) {
-            var ip = 'null';
-            if (response && response.ip) {
-                ip = response.ip;
-            }
-            ipDom = tag('div', 'chrome_the_website_ip', 'chrome_the_website_ip_right', ip)
-            document.body.appendChild(ipDom);
-            ipDom.onmouseover = function() {
-                if (ipDom.className === 'chrome_the_website_ip_right') {
-                    ipDom.className = 'chrome_the_website_ip_left';
-                }
-                else {
-                    ipDom.className = 'chrome_the_website_ip_right';
-                }
-            };
-            // 完成之后，请求归属地数据
-            if (ip && (ip != 'null'))
-            requestIPLocation(ip);
-        });
+chrome.extension.sendMessage({'get': 'ip'}, function (response) {
+    var ip = 'null';
+    if (response && response.ip) {
+        ip = response.ip;
     }
+    var html = "<span id='chrome_the_website_close' onclick='this.parentNode.parentNode.removeChild(this.parentNode); return false;'>X</span>";
+    html += '服务器IP ' + ip;
+    ipDom = tag('div', 'chrome_the_website_ip', html);
+    document.body.appendChild(ipDom);
+    requestIPLocation(ip);
 });
